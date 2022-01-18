2bffbf17b25760fdbaef71a971b066e8a736f7ba
luorun <luorun@xiaomi.com>
2022-01-18 10:28:06 +0800

    添加回到顶部按钮

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div class="header">
+        <div class="header" id="header">
             <router-link to="/list" class="header-title">
                 <img src="https://www-file.huawei.com/-/media/corporate/images/home/logo/huawei_logo.png" alt="">
             </router-link>
@@ -12,14 +12,49 @@
             </div>
         </div>
         <router-view></router-view>
+        <div v-show="this.show" class="scroll-to-top" @click="handleScroll">△</div>
     </div>
 </template>
 <script>
     export default {
+        data () {
+            return {
+                show: false
+            }
+        },
         computed: {
             cartList () {
                 return this.$store.state.cartList;
             }
+        },
+        mounted () {
+            document.addEventListener('scroll', this.showButton)
+        },
+        beforeDestroy () {
+            document.removeEventListener('scroll', this.showButton)
+        },
+        methods: {
+            showButton () {
+                let oldTop = document.body.scrollTop || document.documentElement.scrollTop;
+                if (oldTop > 300) {
+                    this.show = true;
+                } else {
+                    this.show = false;
+                }
+            },
+            handleScroll () {
+                let timer  = null;
+                cancelAnimationFrame(timer);
+                timer = requestAnimationFrame(function fn() {
+                    let oldTop = document.body.scrollTop || document.documentElement.scrollTop;
+                    if(oldTop > 0) {
+                        scrollTo(0, oldTop - 100);
+                        timer = requestAnimationFrame(fn);
+                    } else {
+                        cancelAnimationFrame(timer);
+                    }    
+                });
+            },
         }
     }
 </script>
